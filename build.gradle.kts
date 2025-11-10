5aba0656332c6a0f097670df352010df34072ffb
a.k.lysenko <a.k.lysenko@tbank.ru>
2025-11-10 16:22:28 +0300

    init project

---
+++ build.gradle.kts
@@ -0,0 +1,121 @@
+import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
+
+plugins {
+    application
+    idea
+
+    id("org.springframework.boot") version "3.5.7"
+    id("io.spring.dependency-management") version "1.1.7"
+
+    id("com.avast.gradle.docker-compose") version "0.17.12"
+
+    id("org.openapi.generator") version "7.13.0"
+
+    kotlin("jvm") version "2.0.20"
+    kotlin("plugin.spring") version "2.0.20"
+}
+
+group = "ru.itmo.dws"
+version = "0.0.1-SNAPSHOT"
+description = "itmo-trip"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+idea {
+    module {
+        generatedSourceDirs.add(file("$rootDir/frontend/src/generated"))
+    }
+}
+
+sourceSets {
+    main {
+        kotlin {
+            srcDir("$buildDir/generated/src/main/kotlin")
+        }
+    }
+}
+
+application {
+    mainClass.set("ru.itmo.dws.itmotrip.ItmoTripApplication")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    runtimeOnly("org.postgresql:postgresql")
+}
+
+dependencies {
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+val generateApi = tasks.register<GenerateTask>("generateApi", fun GenerateTask.() {
+    group = "code generation"
+    description = "Generated itmo trip api"
+
+    generatorName.set("kotlin-spring")
+    inputSpec.set("$rootDir/src/main/resources/openapi/itmo-trip-api-v1.openapi.yaml")
+    outputDir.set("$buildDir/generated")
+    packageName.set("ru.itmo.dws.itmo.trip.generated")
+    ignoreFileOverride.set("$rootDir/.openapi-generator-ignore")
+    validateSpec.set(false)
+    configOptions.set(
+        mapOf(
+            "dateLibrary" to "java8",
+            "annotationLibrary" to "none",
+            "documentationProvider" to "none",
+            "exceptionHandler" to "false",
+            "serializationLibrary" to "jackson",
+            "useSpringBoot3" to "true",
+            "useSwaggerUI" to "false",
+            "openApiNullable" to "false",
+            "useTags" to "true",
+            "enumPropertyNaming" to "UPPERCASE",
+            "library" to "spring-boot",
+            "delegatePattern" to "true",
+            "apiSuffix" to "Api",
+        )
+    )
+})
+
+val generateFront = tasks.register<GenerateTask>("generateFront", fun GenerateTask.() {
+    group = "code generation"
+    description = "Generated itmo trip frontend api"
+
+    generatorName.set("typescript-fetch")
+    inputSpec.set("$rootDir/src/main/resources/openapi/itmo-trip-api-v1.openapi.yaml")
+    outputDir.set("$rootDir/frontend/src/generated/openapi")
+    ignoreFileOverride.set("$rootDir/frontend/.openapi-generator-ignore")
+    templateDir.set("$rootDir/frontend/mustache-templates")
+    validateSpec.set(false)
+    configOptions.set(
+        mapOf(
+            "supportsES6" to "true"
+        )
+    )
+})
+
+tasks.compileKotlin.get().dependsOn(generateApi, generateFront)
---
+++ src/test/kotlin/ru/itmo/dws/itmotrip/ItmoTripApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.itmo.dws.itmotrip
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ItmoTripApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

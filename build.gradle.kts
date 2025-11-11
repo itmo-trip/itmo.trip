685d8e645a2843756c36a095cc98dc37a2d8003f
ar.r.lysenko <ar.r.lysenko@tinkoff.ru>
2025-11-11 03:34:04 +0300

    feat: add resource server security configuration with itmo issuer

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.jetbrains.kotlin:kotlin-reflect")

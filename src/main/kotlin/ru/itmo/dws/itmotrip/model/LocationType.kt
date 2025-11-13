c7b3ce6148df05f8e94b96c788c30c772eb28515
Yuri Ufimtsev <ufimtsev3007@gmail.com>
2025-11-14 02:05:43 +0300

    fix: add entity annotations

--- src/main/kotlin/ru/itmo/dws/itmotrip/model/LocationType.kt
+++ src/main/kotlin/ru/itmo/dws/itmotrip/model/LocationType.kt
@@ -1,6 +1,11 @@
 package ru.itmo.dws.itmotrip.model
 
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+
+@Table("location_types")
 data class LocationType(
+    @Id
     val id: Long,
     val code: String
 )

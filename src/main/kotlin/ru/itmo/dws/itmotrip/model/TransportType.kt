866eba0db1439f80d77b83da7004aae96ac25ea6
Yuri Ufimtsev <91129832+YuriUfimtsev@users.noreply.github.com>
2025-11-14 02:06:29 +0300

    fix: add entity annotations (#28)

--- src/main/kotlin/ru/itmo/dws/itmotrip/model/TransportType.kt
+++ src/main/kotlin/ru/itmo/dws/itmotrip/model/TransportType.kt
@@ -1,6 +1,11 @@
 package ru.itmo.dws.itmotrip.model
 
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+
+@Table("location_types")
 data class TransportType (
+    @Id
     val id: Long,
     val logoUrl: String?,
     val nameRu: String

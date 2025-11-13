8a6159504d3303e0ee988bb5c05671fa7ab450a0
quicklybly <96150280+quicklybly@users.noreply.github.com>
2025-11-13 19:29:24 +0300

    fix: try to fix frontend-deploy (#25)
    Co-authored-by: ar.r.lysenko <ar.r.lysenko@tinkoff.ru>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const target = process.env.VITE_API_TARGET
 
-  console.log(env.VITE_API_TARGET)
+  console.log(target)
 
   return {
     plugins: [react()],
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => {
       port: 5177,
       proxy: {
         '/itmo-trip': {
-          target: env.VITE_API_TARGET,
+          target: target,
           changeOrigin: true,
           secure: false,
         },

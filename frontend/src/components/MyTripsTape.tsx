076eb468fb0b76666418085384a5da5cd5f68a49
Yuri Ufimtsev <ufimtsev3007@gmail.com>
2025-11-13 15:23:15 +0300

    implement my_trips layout

--- frontend/src/components/MyTripsTape.tsx
+++ frontend/src/components/MyTripsTape.tsx
@@ -1,10 +1,12 @@
 import Box from '@mui/material/Box';
-import Filter from "./Filter.tsx";
-import Trip from "./Trip.tsx";
-import { trips } from "../TestData.ts";
+import {trips} from "../TestData.ts";
 import Masonry from '@mui/lab/Masonry';
 import {useEffect} from "react";
 import {useAppBarAction} from "../AppBarContext.tsx";
+import MyTrip from "./MyTrip.tsx";
+import {UserInfo} from "./UserInfo.tsx";
+import Grid from "@mui/material/Grid";
+import {Button} from "@mui/material";
 
 export default function MyTripsTape() {
 
@@ -17,7 +19,7 @@ export default function MyTripsTape() {
     // }
 
 
-    const { setAction,  reset } = useAppBarAction();
+    const {setAction, reset} = useAppBarAction();
     useEffect(() => {
         setAction({
             actionName: "Все объявления",
@@ -28,18 +30,47 @@ export default function MyTripsTape() {
     }, [setAction, reset]);
 
     return (
-        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 4 }}>
-            <Filter/>
-            <Box sx={{ width: '100%' }}>
+        <Box sx={{display: 'flex', flexDirection: 'column', gap: 2, width: '100%'}}>
+            <Grid container spacing={2} justifyContent="space-between" marginRight={2}>
+                <Grid size={{md: 6, xs: 12}} sx={{ order: { xs: 2, md: 1 } }}>
+                    <Button
+                        variant="outlined"
+                        onClick={() => console.log('Create trip')}
+                        sx={{
+                            height: '100%',
+                            width: '100%',
+                            borderRadius: 2,
+                            fontSize: 13
+                        }}
+                    >
+                        Создать объявление
+                    </Button>
+                </Grid>
+                <Grid size={{md: 6, xs: 12}} sx={{ order: { xs: 1, md: 2 } }}>
+                    <UserInfo author={trips[0].author}/>
+                </Grid>
+            </Grid>
+
+
+            <Box sx={{width: '100%', marginRight: 12}}>
                 <Masonry
-                    columns={{ xs: 1, md: 2 }}
+                    columns={{xs: 1, md: 2}}
                     spacing={2}
                 >
-                        {trips.map((tr, index) => (
-                            <div key={index}>
-                                <Trip tripData={tr} />
-                            </div>
-                        ))}
+                    <div key={0}>
+                        <MyTrip tripData={trips[0]}/>
+                    </div>
+                    <div key={1}>
+                        <MyTrip tripData={trips[0]}/>
+                    </div>
+                    <div key={2}>
+                        <MyTrip tripData={trips[0]}/>
+                    </div>
+                    {/*{trips.map((tr, index) => (*/}
+                    {/*    <div key={index}>*/}
+                    {/*        <Trip tripData={tr} />*/}
+                    {/*    </div>*/}
+                    {/*))}*/}
                 </Masonry>
             </Box>
         </Box>
